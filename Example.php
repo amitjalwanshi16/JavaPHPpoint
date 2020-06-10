6c89bf433eea0a4693a018a32ffd041f27cee1a9
amitjalwanshi16 <44563601+amitjalwanshi16@users.noreply.github.com>
2020-06-10 16:44:16 +0530

    Add files via upload

---
+++ Example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Number</title>
+</head>
+<body>
+<?php
+// for ($i=0;$i<=30;$i++) {
+//   if ($i%2==0) {
+//     echo " ".$i;
+//   }   
+//   }
+
+$i=0;
+while($i<=30) {
+if ($i%2==0) {
+ echo " ".$i;
+ $i+=1;
+}
+}
+?>
+</body>
+</html>
---
+++ Index2.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JavaPHPpoint</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+</head>
+<body class="bg-light">
+<div class="container-fluid">
+<!-- <nav class="navbar navbar-dark bg-dark sticky-top navbar-expand-lg navbar-expand-md navbar-light navbar-nav navbar-default">
+
+  <a class="navbar-brand" href="#">
+  	
+    <img src="logo.jpg" width="100" height="80" margin-left="10px" class="d-inline-block align-top" alt="">
+       <a href="#" class="navbar-brand" ><h2>Java<span class="text-success">PHP</span>point</h2></a>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto mx-5">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+       <li >
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+       <li >
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+       <li >
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Services
+        </a>
+        <div class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#"> Web Development</a>
+          <a class="dropdown-item" href="#">Android Developement</a>
+          <a class="dropdown-item" href="#">Project Development</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+ -->
+
+<?php include("Header.html");?>
+
+<div class="container bg-white mt-0" style="margin-top:30px">
+  <div class="row">
+
+<!-- left Side of body-->
+  	
+    <div class="col-sm-4 bg-light ">
+    	
+      <ul class="nav nav-pills flex-column d-none d-sm-block d-md-block d-lg-block">
+      <a class="list-group-item">	<h3>Related Topics</h3></a>
+
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+<a href="#" class="list-group-item list-group-item-action">This is a educational website</a>
+
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+ <!--  Right side of body -->
+     <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+
+       <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+
+
+    </div>
+ 
+</div>
+</div>
+</div>
+
+<div class="jumbotron text-center bg-dark text-white" style="margin-bottom:0">
+  <p>Footer</p>
+</div>
+
+
+</body>
+</html>
